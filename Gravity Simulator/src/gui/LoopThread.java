cc33c0899a9282c73b9acf84afa89c0d70d4fad5
Leonardo <Leonardo@Leonardo-PC>
2017-07-03 10:02:09 +0100

    update

--- Gravity Simulator/src/gui/LoopThread.java
+++ Gravity Simulator/src/gui/LoopThread.java
@@ -37,12 +37,12 @@ public class LoopThread extends Thread{
 	        		g.space.update(1/max_fps);
 	        		g.repaint();
 	        		
-	        		if(!(g.pictureNumber>2)){
+	        		/*if(!(g.pictureNumber>7200)){
 	        		  g.takePicture();
 	        		}
 	        		else{
 	        			running=false;
-	        		}
+	        		} */
 	        		
 	        		if(numberOfParticles!=g.space.particles.size()){
 	        			numberOfParticles=g.space.particles.size();
